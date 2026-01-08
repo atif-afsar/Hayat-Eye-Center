1ce76bef00dde139d34f6f917dadbd938d4c5e6b
Atif Afsar <atifafsar70@gmail.com>
2026-01-08 12:17:04 +0530

    Applying lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, lazy, Suspense } from "react";
 
-import Home from "./Pages/Home";
-import About from "./Pages/About";
 import Navbar from "./Components/Home/Navbar";
 import Footer from "./Components/Home/Footer";
-import Service from "./Pages/Service";
-import Doctors from "./Pages/Doctors";
-import Testimonials from "./Pages/Testimonials";
-import Contact from "./Pages/Contact";
 
 import ScrollToTop from "./Pages/ScrollToTop";
 import ScrollToTopOnRouteChange from "./ScrollToTopOnRouteChange";
 import SmoothScroll from "./Pages/SmoothScroll";
 import WhatsAppButton from "./Pages/WhatsAppButton";
 import Loader from "./Pages/Loader";
+
+/* Lazy Pages */
+const Home = lazy(() => import("./Pages/Home"));
+const About = lazy(() => import("./Pages/About"));
+const Service = lazy(() => import("./Pages/Service"));
+const Doctors = lazy(() => import("./Pages/Doctors"));
+const Testimonials = lazy(() => import("./Pages/Testimonials"));
+const Contact = lazy(() => import("./Pages/Contact"));
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -23,7 +25,6 @@ export default function App() {
     const timer = setTimeout(() => {
       setLoading(false);
     }, 1000); // smooth medical feel
-
     return () => clearTimeout(timer);
   }, []);
 
@@ -36,14 +37,17 @@ export default function App() {
       <ScrollToTop />
 
       <SmoothScroll>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Service />} />
-          <Route path="/doctors" element={<Doctors />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        {/* LAZY ROUTES */}
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Service />} />
+            <Route path="/doctors" element={<Doctors />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
 
         <WhatsAppButton />
       </SmoothScroll>
